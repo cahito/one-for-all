48df6c665839557d685508956e7b19c6cc7663e0
cahito <cahito@gmail.com>
2022-06-05 11:25:16 -0300

    Feat - Requisito cinco cumprido

---
+++ desafio5.sql
@@ -0,0 +1,9 @@
+SELECT
+	tr.track AS cancao,
+    COUNT(rh.track_id) AS reproducoes
+FROM tracks AS tr
+INNER JOIN reproduction_history AS rh
+ON rh.track_id=tr.id
+GROUP BY tr.track
+ORDER BY reproducoes DESC, cancao
+LIMIT 2;
