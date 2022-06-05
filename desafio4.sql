af324b059947c43fa495629ffc090717749d3fba
cahito <cahito@gmail.com>
2022-06-05 11:19:05 -0300

    Feat - Requisito quatro cumprido

---
+++ desafio4.sql
@@ -0,0 +1,8 @@
+SELECT
+	DISTINCT u.name AS usuario,
+    IF(MAX(rh.reproduction_date) >= '2021-01-01 00:00:00', 'Usuário ativo', 'Usuário inativo') AS condicao_usuario
+FROM users AS u
+	INNER JOIN reproduction_history AS rh
+	ON u.id=rh.user_id
+GROUP BY u.name
+ORDER BY u.name;
